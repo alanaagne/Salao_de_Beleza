5c18d9cf2460d454d74ef9dfcc27d6d0fac08125
ijoaom <joaomanoel2020.1@gmail.com>
2025-10-19 17:49:45 -0300

    Adição de rota para a tela inicial a partir de  funcionário e cliente

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,8 +5,8 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: 'localhost', // Mude se seu BD estiver em outro lugar
-    user: '', // mude para Seu usuário do MySQL
-    password: '', // Sua senha do MySQL
+    user: 'root', // mude para Seu usuário do MySQL
+    password: 'admin', // Sua senha do MySQL
     database: 'ExpressoDaBeleza', // O nome do banco que você criou
     waitForConnections: true,
     connectionLimit: 10,
